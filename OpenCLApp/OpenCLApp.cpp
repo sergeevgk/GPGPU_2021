96b1949052f8b9549b6d17a56f5f46efa107ac6a
georgii <gosha199904@gmail.com>
2021-03-15 13:35:26 +0300

    Add execution time information; Fix codestyle

--- OpenCLApp/OpenCLApp.cpp
+++ OpenCLApp/OpenCLApp.cpp
@@ -6,20 +6,16 @@
 #include <CL/cl.h>
 #include <cstdlib>
 #include <iostream>
+#include <Windows.h>
 
 #pragma warning(disable: 4996)
 #define eps 1e-6
 
-#define SIZE_M 100
-#define SIZE_N 100
-#define SIZE_P 100
+#define SIZE_M 1000
+#define SIZE_N 1000
+#define SIZE_P 1000
 
-//define function 
-float* init_matrix(float* A, cl_int row, cl_int col, int value);
-void print_matrix(float* A, cl_int row, cl_int col);
-int  check_result(float* res, float* c_res, float* a, float* b, cl_int m, cl_int n, cl_int p);
-
-float* init_matrix(float* A, cl_int row, cl_int col, float value) {
+float* InitialiseMatrix(float* A, cl_int row, cl_int col, float value) {
     int i = 0;
     for (i = 0; i < row * col; i++) {
         if (value == 0)
@@ -29,7 +25,7 @@ float* init_matrix(float* A, cl_int row, cl_int col, float value) {
     }
     return A;
 }
-void print_matrix(float* A, cl_int row, cl_int col) {
+void PrintMatrix(float* A, cl_int row, cl_int col) {
     int i = 0;
     for (i = 0; i < row * col; i++) {
         if (i % col == 0)
@@ -38,7 +34,7 @@ void print_matrix(float* A, cl_int row, cl_int col) {
     }
     printf("\n================\n");
 }
-int  check_result(float* res, float* c_res, float* a, float* b, cl_int m, cl_int n, cl_int p) {
+int  CheckResult(float* res, float* c_res, float* a, float* b, cl_int m, cl_int n, cl_int p) {
     int i, k, error = 0;
     for (i = 0; i < m * p; i++) {
         int div_i = i / p;
@@ -54,7 +50,7 @@ int  check_result(float* res, float* c_res, float* a, float* b, cl_int m, cl_int
     return error;
 }
 
-cl_program load_program(cl_context context, cl_device_id device, const char* filename)
+cl_program LoadProgram(cl_context context, cl_device_id device, const char* filename)
 {
     FILE* fp;
     fopen_s(&fp, filename, "rt");
@@ -103,6 +99,23 @@ void Release(cl_kernel kernelFunction,
 
 }
 
+double GetEventExecutionTime(cl_event event)
+{
+    cl_ulong start, end;
+    clGetEventProfilingInfo(event,
+        CL_PROFILING_COMMAND_START,
+        sizeof(cl_ulong),
+        &start,
+        NULL);
+    clGetEventProfilingInfo(event,
+        CL_PROFILING_COMMAND_END,
+        sizeof(cl_ulong),
+        &end,
+        NULL);
+    double execTime = (end - start) * 1e-6;
+    return execTime;
+}
+
 int main(int argc, char** argv)
 {
     cl_int err = 0;
@@ -113,25 +126,25 @@ int main(int argc, char** argv)
     cl_device_id* devices = NULL;
     cl_command_queue queue = 0;
     cl_program program = 0;
-    cl_mem cl_a = 0, cl_b = 0, cl_res = 0;
+    cl_mem clBufferA = 0, clBufferB = 0, clBufferResult = 0;
     cl_kernel kernelFunction = 0;
     cl_event event;
-    int num_total_devices = 0;
-    char devname[16][256] = { {0} };
-    size_t cb, work_size;
+    int numTotalDevices = 0;
+    char devName[16][256] = { {0} };
+    size_t cb, workSize;
 
     const cl_int m = SIZE_M, n = SIZE_N, p = SIZE_P;
-    cl_float* a, *b, *res, *cpu_res;
-    a = (cl_float*)malloc(m * n * sizeof(cl_float));
-    b = (cl_float*)malloc(n * p * sizeof(cl_float));
-    res = (cl_float*)malloc(m * p * sizeof(cl_float));
+    cl_float* matrixA, *matrixB, *matrixResult, *matrixCpuResult;
+    matrixA = (cl_float*)malloc(m * n * sizeof(cl_float));
+    matrixB = (cl_float*)malloc(n * p * sizeof(cl_float));
+    matrixResult = (cl_float*)malloc(m * p * sizeof(cl_float));
+    matrixCpuResult = (cl_float*)malloc(m * p * sizeof(cl_float));
     int resultCorrectness = 0;
-    cpu_res = (cl_float*)malloc(m * p * sizeof(cl_float));
     //Initialize matrices
-    init_matrix(a, m, n, 1.0f);
-    init_matrix(b, n, p, 2.0f);
-    init_matrix(res, m, p, 0.0f);
-    init_matrix(cpu_res, m, p, 0.0f);
+    InitialiseMatrix(matrixA, m, n, 1.0f);
+    InitialiseMatrix(matrixB, n, p, 2.0f);
+    InitialiseMatrix(matrixResult, m, p, 0.0f);
+    InitialiseMatrix(matrixCpuResult, m, p, 0.0f);
 
     if (clGetPlatformIDs(0, 0, &num) != CL_SUCCESS) {
         printf("Unable to get platforms.\n");
@@ -155,7 +168,7 @@ int main(int argc, char** argv)
     context = clCreateContextFromType(prop, CL_DEVICE_TYPE_ALL, NULL, NULL, NULL);
     if (context == 0) {
         printf("Can't create OpenCL context.\n");
-        Release(kernelFunction, program, cl_a, cl_b, cl_res, queue, context);
+        Release(kernelFunction, program, clBufferA, clBufferB, clBufferResult, queue, context);
         return -1;
     }
     
@@ -164,30 +177,30 @@ int main(int argc, char** argv)
     clGetContextInfo(context, CL_CONTEXT_DEVICES, cb, devices, 0);
     if (cb == 0) {
         printf("Can't get devices.\n");
-        Release(kernelFunction, program, cl_a, cl_b, cl_res, queue, context);
+        Release(kernelFunction, program, clBufferA, clBufferB, clBufferResult, queue, context);
         return -1;
     }
-    num_total_devices = cb / sizeof(cl_device_id);
+    numTotalDevices = cb / sizeof(cl_device_id);
     const cl_queue_properties queueProperies = CL_QUEUE_PROFILING_ENABLE;
     //Specify the queue
     queue = clCreateCommandQueue(context, devices[0], queueProperies, 0);
     if (queue == 0) {
         printf("Can't create command queue.\n");
-        Release(kernelFunction, program, cl_a, cl_b, cl_res, queue, context);
+        Release(kernelFunction, program, clBufferA, clBufferB, clBufferResult, queue, context);
         return -1;
     }
-    program = load_program(context, devices[0], "program.cl");
+    program = LoadProgram(context, devices[0], "program.cl");
     if (program == 0) {
         printf("Can't build program.\n");
-        Release(kernelFunction, program, cl_a, cl_b, cl_res, queue, context);
+        Release(kernelFunction, program, clBufferA, clBufferB, clBufferResult, queue, context);
         return -1;
     }
-    cl_a = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_float) * m * n, a, NULL);
-    cl_b = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_float) * n * p, b, NULL);
-    cl_res = clCreateBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_USE_HOST_PTR, sizeof(cl_float) * m * p, res, NULL);
-    if (cl_a == 0 || cl_b == 0 || cl_res == 0) {
+    clBufferA = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_float) * m * n, matrixA, NULL);
+    clBufferB = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_float) * n * p, matrixB, NULL);
+    clBufferResult = clCreateBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_USE_HOST_PTR, sizeof(cl_float) * m * p, matrixResult, NULL);
+    if (clBufferA == 0 || clBufferB == 0 || clBufferResult == 0) {
         printf("Can't create OpenCL buffers.\n");
-        Release(kernelFunction, program, cl_a, cl_b, cl_res, queue, context);
+        Release(kernelFunction, program, clBufferA, clBufferB, clBufferResult, queue, context);
         return 1;
     }
 
@@ -195,35 +208,44 @@ int main(int argc, char** argv)
 
     if (kernelFunction == 0) {
         printf("Can't load kernel.\n");
-        Release(kernelFunction, program, cl_a, cl_b, cl_res, queue, context);
+        Release(kernelFunction, program, clBufferA, clBufferB, clBufferResult, queue, context);
         return -1;
     }
-    clSetKernelArg(kernelFunction, 0, sizeof(cl_mem), &cl_a);
-    clSetKernelArg(kernelFunction, 1, sizeof(cl_mem), &cl_b);
-    clSetKernelArg(kernelFunction, 2, sizeof(cl_mem), &cl_res);
+    clSetKernelArg(kernelFunction, 0, sizeof(cl_mem), &clBufferA);
+    clSetKernelArg(kernelFunction, 1, sizeof(cl_mem), &clBufferB);
+    clSetKernelArg(kernelFunction, 2, sizeof(cl_mem), &clBufferResult);
     clSetKernelArg(kernelFunction, 3, sizeof(cl_int), &m);
     clSetKernelArg(kernelFunction, 4, sizeof(cl_int), &n);
     clSetKernelArg(kernelFunction, 5, sizeof(cl_int), &p);
     // number of work items == number of elements in res matrix
-    work_size = m * p;
+    workSize = m * p;
     // runs kernel worksize times => one work item counts one element in res matrix
-    if (clEnqueueNDRangeKernel(queue, kernelFunction, 1, 0, &work_size, 0, 0, 0, &event) != CL_SUCCESS)
+    if (clEnqueueNDRangeKernel(queue, kernelFunction, 1, 0, &workSize, 0, 0, 0, &event) != CL_SUCCESS)
         printf("Can't enqueue kernel.\n");
 
-    if(clEnqueueReadBuffer(queue, cl_res, CL_TRUE, 0, sizeof(cl_float) * work_size, res, 0, 0, 0) != CL_SUCCESS)
+    if(clEnqueueReadBuffer(queue, clBufferResult, CL_TRUE, 0, sizeof(cl_float) * workSize, matrixResult, 0, 0, 0) != CL_SUCCESS)
         printf("Can't enqueue read buffer.\n");
 
     clWaitForEvents(1, &event);
+    printf("GPU execution time: %.04lf ms\n\n", GetEventExecutionTime(event));
 
     clFinish(queue);
     // check results
     err = 0;
-    resultCorrectness = check_result(res, cpu_res, a, b, m, n, p);
+    LARGE_INTEGER frequency;
+    QueryPerformanceFrequency(&frequency);
+    LARGE_INTEGER start;
+    QueryPerformanceCounter(&start);
+    resultCorrectness = CheckResult(matrixResult, matrixCpuResult, matrixA, matrixB, m, n, p);
+    LARGE_INTEGER end;
+    QueryPerformanceCounter(&end);
+
+    printf("CPU execution time: %.04lf ms\n\n", ((double)(end.QuadPart - start.QuadPart) / frequency.QuadPart) * 1000);
     if (resultCorrectness == -1) 
         printf("Matrix multiplication has errors.\n");
     else 
         printf("Matrix multiplication is correct.\n");
-    printf("Press any key...");
+    printf("Press any key...\n");
     std::cin.get();
     // print results
     //print_matrix(a, m, n);
@@ -231,7 +253,7 @@ int main(int argc, char** argv)
     //print_matrix(res, m, p);
     //print_matrix(cpu_res, m, p);
 
-    Release(kernelFunction, program, cl_a, cl_b, cl_res, queue, context);
+    Release(kernelFunction, program, clBufferA, clBufferB, clBufferResult, queue, context);
 
     return 0;
 }
